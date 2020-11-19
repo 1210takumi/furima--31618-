a897fd9be21c0b8376be2431858d441795eb9ec5
1210takumi <uw.t.t.t.takumi@icloud.com>
2020-11-19 10:40:42 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,7 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
+    current_user.id == @item.user.id
     item.destroy
     redirect_to root_path
   end
